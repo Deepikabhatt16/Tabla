1f1fe9325ea8c193462181fcda74828c3af173e0
DEEPIKA_BHATT 19 <114798298+Deepikabhatt16@users.noreply.github.com>
2023-09-09 01:19:52 +0530

    Add files via upload

---
+++ Play-Tabla/tabla.js
@@ -0,0 +1,30 @@
+
+document.querySelector(".w").addEventListener("click",function (){
+    var tom1=new Audio("tracks/beat-1.mp3");
+    tom1.play();
+  });
+  
+  document.querySelector(".a").addEventListener("click",function (){
+    var tom2=new Audio("tracks/beat-2.mp3");
+    tom2.play();
+  });
+  document.querySelector(".s").addEventListener("click",function (){
+    var tom3=new Audio("tracks/beat-3.mp3");
+    tom3.play();
+  });
+  document.querySelector(".d").addEventListener("click",function (){
+    var tom4=new Audio("tracks/beat-4.mp3");
+    tom4.play();
+  });
+  document.querySelector(".j").addEventListener("click",function (){
+    var kickbass=new Audio("tracks/beat-5.wav");
+    kickbass.play();
+  });
+  document.querySelector(".k").addEventListener("click",function (){
+    var snare=new Audio("tracks/beat-6.mp3");
+    snare.play();
+  });
+  document.querySelector(".l").addEventListener("click",function (){
+    var crash=new Audio("tracks/beat-7.mp3");
+    crash.play();
+  });
